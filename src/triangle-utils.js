e48a55073d338a7ead9de6ad31a90da355da850c
Alexander Belopolsky <a@enlnt.com>
2020-12-04 20:06:15 -0500

    implement doubleTriangle

--- src/triangle-utils.js
+++ src/triangle-utils.js
@@ -26,6 +26,20 @@ export function xyFromAB(a: number, b: number): {x: number, y: number} {
   return {x, y};
 };
 
+export function doubleTriangle(P: CubicPoint): CubicPoint {
+  const x = P.a;
+  const y = P.b;
+  const Fx = 3 * x**2 + 2 * x * y + y ** 2 + 2 * x + y - 1
+  const Fy = 3 * y**2 + 2 * x * y + x ** 2 + 2 * y + x - 1
+  const c2 = Fx**2*x + 3*Fx**2*y + Fx**2 - 2*Fx*Fy*x - 2*Fx*Fy*y - Fx*Fy + 3*Fy**2*x + Fy**2*y + Fy**2
+  const c3 = -(Fx**3) + Fx**2*Fy - Fx*Fy**2 + Fy**3
+  const t = -c2/c3
+  return {
+    a: y - t * Fx,
+    b: x + t * Fy,
+  }
+}
+
 export function addTriangles(P: CubicPoint, Q: CubicPoint): CubicPoint {
   const a1 = P.a;
   const b1 = P.b;
